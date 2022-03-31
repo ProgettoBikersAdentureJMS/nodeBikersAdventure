25e452daa608ee9d191d14915efd40885026ec90
SamuelBanfi <samuel.banfi@samtrevano.ch>
2022-03-31 11:27:17 +0200

    Finita la gestione dei gruppi

---
+++ src/components/GroupInfo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <hr>
+        <h2>Partecipanti:</h2>
+        <h3 v-for="partecipant, itemIndex in partecipants" :key="itemIndex"> 
+            {{ partecipant }}
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                default: "Nome"
+            },
+            partecipants: {
+                type: Array,
+                default: []
+            }
+        }   
+    }
+</script>
--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -1,19 +1,87 @@
 <template>
     <div>
         <h1>Gruppi</h1>
-        <CreateGroup />
-        <GroupsList />
+        <div id="groups-container">
+            <div id="list-groups">
+                <div id="add-group">
+                    <button id="add-image" @click="showAddGroup">
+                        <img :src="plusIcon">
+                        <p>Crea un gruppo</p>
+                    </button>
+                </div>
+                <GroupsList @getName="getName" />
+            </div>
+            <div id="group-info">
+                <GroupInfo 
+                v-bind:style="{display: infoDisplay}"
+                :name="name"
+                :partecipants="partecipants" />
+                <CreateGroup v-bind:style="{display: createDisplay}" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import CreateGroup from "../components/CreateGroup.vue"
     import GroupsList from "../components/GroupsList.vue"
+    import GroupInfo from "../components/GroupInfo.vue"
+    import { collection, getDocs, getFirestore } from '@firebase/firestore'
+    import plusIcon from "../assets/plus.png"
 
     export default {
         components: {
             CreateGroup,
-            GroupsList
-        }   
+            GroupsList,
+            GroupInfo
+        },
+        data() {
+            return {
+                plusIcon,
+                infoDisplay: '',
+                createDisplay: 'none',
+                snapshotGroups: getDocs(collection(getFirestore(), "gruppi")),
+                name: '',
+                partecipants: []
+            }
+        },
+        methods: {
+            showAddGroup() {
+                this.createDisplay = ""
+                this.infoDisplay = "none"
+            },
+            getName(value) {
+                this.createDisplay = "none"
+                this.infoDisplay = ""
+
+                this.name = value
+                
+                this.snapshotGroups.then(groups => {
+                    groups.forEach(group => {
+                        var groupData = group.data()
+
+                        if (groupData.nome == this.name) {
+                            this.partecipants = groupData.partecipanti
+                        }
+                    })
+                })
+            }
+        }
     }
 </script>
+
+<style>
+    #groups-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    #list-groups, #group-info {
+        flex: 1;
+    }
+
+    #group-info {
+        flex: 1;
+    }
+</style>
